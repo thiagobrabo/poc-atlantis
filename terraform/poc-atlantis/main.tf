4f981fc79cfdb255d977accb45eeffe9310c9031
Thiago Brabo <thiago.dbrabo@gmail.com>
2024-04-03 14:12:05 -0300

    feat:creating nem file

--- terraform/poc-atlantis/main.tf
+++ terraform/poc-atlantis/main.tf
@@ -3,3 +3,7 @@ resource "local_file" "arquivo_simples" {
   filename = "${path.module}/arquivo-01.txt"  # Caminho e nome do arquivo a ser criado
   content  = "Este é um arquivo criado pelo Terraform, para execução de testes.\n"  # Conteúdo do arquivo
 }
+resource "local_file" "arquivo_simples_2" {
+  filename = "${path.module}/arquivo-02.txt"  # Caminho e nome do arquivo a ser criado
+  content  = "Este é um arquivo criado pelo Terraform, para execução de testes.\n"  # Conteúdo do arquivo
+}
